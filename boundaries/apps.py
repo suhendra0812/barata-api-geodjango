4a111e0654b39996e917760dcbabfe4ca8063684
Suhendra <suhendra0812@gmail.com>
2021-04-18 13:15:19 +0800

    chore: fix formatting and linting

--- boundaries/apps.py
+++ boundaries/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class BoundariesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'boundaries'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "boundaries"
